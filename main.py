a222784bc28a521b4911ce8a0e5c514fe20e8328
Juan Cruz <jc23729@gmail.com>
2020-12-04 16:46:45 +0000

    Got total items, in list, Generate random numbers 0 to last index

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
@@ -5,5 +7,12 @@ names = names_string.split(", ")
 
 #Write your code below this line 👇
 
+#Get the total num of items in a list
+num_items = len(names)
+#we did import random up top
+#Generate random numbers between 0 and the last index and turned it into a variable with random_choice
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay_bill= names[random_choice]
 
-
+print(person_who_will_pay_bill + " is going to buy the meal today!")
